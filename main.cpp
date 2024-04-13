ec95edd3dece7da56b8c405268ac24a017e1604c
squawk7x <andreas@ottburg.de>
2024-04-13 12:26:37 +0200

    pause Project Watch

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "watch.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // qDebug() << "hola mundo";
+    // qInfo() << "Hello World";
+    // qWarning() << "Warning";
+    // qCritical() << "Critical";
+    // qFatal() << "Fatal";
     Watch w;
     w.show();
     return a.exec();
